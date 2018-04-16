0dff9741b400e817ac44246f513485f74ffc2a3a
msaada88 <38444458+msaada88@users.noreply.github.com>
2018-04-16 16:59:46 -0700

    Update Assignment 1.cpp

--- CS110A-C/Assignment 1.cpp
+++ CS110A-C/Assignment 1.cpp
@@ -1,5 +1,12 @@
-#include <iostream>
+ #include <iostream>
+using namespace std;
 
-int main() {
-    std::cout << "Hello World!\n";
-}
+int main()
+{
+    cout<<"      _____ /\\__/\\" << endl;
+    cout <<"    /  (   (  0 0 )"<< endl;
+    cout <<"   /  (   _  = ^ =" << endl ;
+    cout << " *     |_|_| |_|_|"<< endl;
+   cout << "        \" \"   \" \""<< endl;
+}  
+ 
